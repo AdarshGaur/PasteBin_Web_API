ced7b35efd2d801dcf60490d86f4ee8d63420296
Adarsh <adarshgaur3@gmail.com>
2020-09-16 14:50:15 +0530

    added Snippet Serializer

--- pastbin_web_api/settings.py
+++ pastbin_web_api/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     #'snippets',
-    'snippets.app.SnippetsConfig',
+    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
